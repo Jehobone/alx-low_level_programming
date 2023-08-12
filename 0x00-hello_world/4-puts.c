eda5e66d9cf126ca872c05840d64f211b28c538f
Jehobone <jehoshaphatbonsu3@gmail.com>
2023-08-12 03:25:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#iclude <studio.h>
-
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- * /
-   int main(void)
-   {
-           puts(''\''programming is like building a multilingual puzzle'');
-	   return (0);
-	}   
+ * Return always 0 (success)
+ */
+int main(void)
+{
+	puts(''/''programming is like building a multilingual puzzle'');
+	return (0);
+}
